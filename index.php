b33ef692ab0854ae1b9cf039cb5dedd55a793fed
KarlisKalkis <kkalkis21@gmail.com>
2023-09-29 11:37:20 +0300

    Index page done

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include 'includeformainpages/header.php'?>
+
+<body>
+    
+
+    <!-- Header Section -->
+    <header>
+        <h1>Welcome to Danielas Beauty</h1>
+        <p>Your Ultimate Beauty Experience Awaits</p>
+    </header>
+    
+    <!-- About Us Section -->
+    <section id="about" class="container text-center">
+        <h2 class="section-title">About Us</h2>
+        <div class="section-content">
+            <p>Danielas Beauty is more than just a beauty salon; it's a place where you can transform yourself and enhance your natural beauty. Our team of skilled professionals is dedicated to providing you with the finest beauty treatments and products. Come and experience luxury and relaxation like never before.</p>
+        </div>
+    </section>
+    
+    <!-- Procedures Section -->
+    <section id="procedures" class="container text-center">
+        <h2 class="section-title">Our Procedures</h2>
+        <div class="section-content">
+            <p>Indulge in a wide range of beauty services, including facials that rejuvenate your skin, luxurious manicures and pedicures, hair styling that matches your unique personality, and waxing that leaves you feeling smooth and confident. Discover the beauty in every detail.</p>
+        </div>
+    </section>
+    
+    <!-- Products Section -->
+    <section id="products" class="container text-center">
+        <h2 class="section-title">Shop Beauty Products</h2>
+        <div class="section-content">
+            <p>Explore our exclusive collection of beauty products carefully curated to meet your skincare and haircare needs. From premium skincare essentials to haircare wonders, we offer the best products to keep you looking and feeling your best, even at home.</p>
+        </div>
+    </section>
+    
+    <!-- Footer Section -->
+    <footer>
+        <p>&copy; 2023 Danielas Beauty</p>
+    </footer>
+
+    <!-- Include Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
