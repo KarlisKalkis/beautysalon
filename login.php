3957f5f6bf5b8ccc2306e3c9aeeeb190633f0977
KarlisKalkis <kkalkis21@gmail.com>
2023-09-30 11:01:30 +0300

    Made login page

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php include 'includeformainpages/header.php'?>
+
+
+<section id="login" class="container text-center my-5">
+    <h2 class="section-title">Login</h2>
+    <div class="section-content">
+        <form>
+            <div class="form-group">
+                <label for="username">E-mail:</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter your username">
+            </div>
+            <div class="form-group mt-5">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+            </div>
+            <button type="submit" class="btn btn-primary mt-5">Login</button>
+            <p class="pb-lg-5 mt-5" style="color: #393f81;">Don't have an account: <a href="user_register.php" style="color: #393f81;">Register here</a></p>
+
+    </form>
+    </div>
+    
+</section>
