97919bf343c382e768d7d11d7309308e5434e67d
[Students] Karlis Kalkis <04DPKKalkis@rvt.lv>
2023-10-02 14:49:49 +0300

    about us page startted to create

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php include 'includeformainpages/header.php'?>
+
+    <!-- About Us Page -->
+    <div class="container my-5">
+        <h1 class="text-center mb-4">About Us</h1>
+        <div class="section-content">
+            <p>Danielas Beauty is more than just a beauty salon; it's a place where you can transform yourself and enhance your natural beauty. Our team of skilled professionals is dedicated to providing you with the finest beauty treatments and products. Come and experience luxury and relaxation like never before.</p>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <h1 class="text-center mb-4">How we started</h1>
+        <div class="section-content">
+            <p>We started as single person who was dedicated to style and fashion. And why not to combine that with useful and teach other people about that? So thats why with hard work and dedication we have our company</p>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <h1 class="text-center mb-4">Products that we have and procedures that we provide</h1>
+        <div class="section-content">
+            <p>Procedures are from hairstyling to styling clothes</p>
+        </div>
+    </div>
+    
+    <!-- Footer Section -->
+    <footer class="bg-dark text-white text-center py-3">
+        &copy; 2023 Danielas Beauty
+    </footer>
+
+    <!-- Include Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
